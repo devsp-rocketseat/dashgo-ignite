f9c136d3340124cfffce7c178f8df9584bd7e83e
DevSP <contatodevsp@gmail.com>
2021-04-10 16:09:26 -0300

    Fluxo de navegação

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,16 +1,20 @@
 import { ElementType } from 'react'
-import { Icon, Link, Text, LinkProps } from '@chakra-ui/react'
+import { Icon, Link as Chakralink, Text, LinkProps } from '@chakra-ui/react'
+import Link from 'next/link'
 
 interface NavLinkProps extends LinkProps {
   icon: ElementType
   children: string
+  href: string
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">{children}</Text>
+    <Link href={href} passHref>
+      <Chakralink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </Chakralink>
     </Link>
   )
 }
