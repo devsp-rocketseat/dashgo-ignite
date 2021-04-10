ba5067ac4b6f33c01d84880f12e076fb9c22c7d7
DevSP <contatodevsp@gmail.com>
2021-04-10 15:06:00 -0300

    Sidebar responsiva

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,36 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
 
 import { Logo } from './Logo'
 import { SearchBox } from './SearchBox'
 import { NotificationsNav } from './NotificationsNav'
 import { Profile } from './Profile'
 
+import { useSidebarDrawer } from '../../Contexts/SidebarDrawerContext'
+
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
 
   return (
-    <Flex as="header" w="100%" maxW={1480} h="20" mx="auto" mt="4" px="6" align="center">
+    <Flex as="header" w="100vw" maxW={1480} h="20" mx="auto" mt="4" px="6" align="center">
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
+
       <Logo />
+
 
       {isWideVersion && <SearchBox />}
 
