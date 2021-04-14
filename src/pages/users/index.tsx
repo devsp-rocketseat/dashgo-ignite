420e503c639d863db4b25a246e7d4053a688d610
DevSP <contatodevsp@gmail.com>
2021-04-14 19:45:41 -0300

    Componente de paginação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -103,7 +103,11 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={4}
+                onPageChange={() => { }}
+              />
             </>
           )}
         </Box>
